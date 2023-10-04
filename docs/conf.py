afc6a6fa91b6e1199e5ac481b8ff246d384ad616
Andrei Jiroh Halili (RecapTime.dev) <ajhalili2006@crew.recaptime.eu.org>
2023-10-04 21:04:15 +0800

    Setup Sphinx and friends (including custom Wakatime project name)
    On-behalf-of: @recaptime-dev <oss@crew.recaptime.eu.org>
    Signed-off-by: Andrei Jiroh Halili (RecapTime.dev) <ajhalili2006@crew.recaptime.eu.org>

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Squad Policies'
+copyright = 'Recap Time Squad, dedicated to public domain under Creative Commons Zero Universal License'
+author = 'Recap Time Squad'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # External stuff
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
+]
+
+#templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for TODOs -------------------------------------------------------
+#
+
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_title = 'Recap Time Squad Policies'
+html_static_path = ['_static']
